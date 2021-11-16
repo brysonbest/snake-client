4abeb1dc53375b90e6b28bdbba64e915cfc229b1
Bryson Best <bryson.best@gmail.com>
2021-11-15 18:27:50 -0800

    Refactored client and play file behaviours.

--- play.js
+++ play.js
@@ -1,18 +1,4 @@
-const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  conn.setEncoding('utf8');
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
+const connect = require('./client.js');
 
 console.log('Connecting ...');
 connect();
